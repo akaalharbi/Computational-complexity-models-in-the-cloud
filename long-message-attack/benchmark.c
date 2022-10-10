3b2620e8553dea107044cd9e350a1f7913ed6add
Ahmed Alharbi <akaalharbi@protonmail.com>
2022-10-10 14:23:45 +0200

    fast sha256 on intel, 5x faster

--- long-message-attack/benchmark.c
+++ long-message-attack/benchmark.c
@@ -4,6 +4,7 @@
 
 
 #include "sha256.h"
+#include "sha256-x86.h"
 #include <stddef.h>
 #include <stdio.h>
 #include "dict.h"
@@ -19,7 +20,8 @@ int idx_cycle = -1;
 
 
 
-float bench_mark_sha256(){
+float benchmark_sha256(){
+
   size_t n_of_blocks = 1<<25;
   
   struct timeval begin, end;
@@ -61,9 +63,62 @@ float bench_mark_sha256(){
   elapsed = seconds + microseconds*1e-6;
 
   float hash_per_sec = (float) n_of_blocks / elapsed;
-  printf("elapsed=%fsec, %f hash/sec≈2^%f \n", elapsed, hash_per_sec, log2(hash_per_sec));
+  printf("normal sha256:\nelapsed=%fsec, %f hash/sec≈2^%f \n", elapsed, hash_per_sec, log2(hash_per_sec));
   return hash_per_sec;
 }
+
+
+float benchmark_sha256_x86(){
+
+  size_t n_of_blocks = 1<<25;
+  
+  struct timeval begin, end;
+  long seconds = 0;
+  long microseconds = 0;
+  double elapsed = 0;
+
+  BYTE M[64] = {0}; // long_message_zeros(n_of_blocks*512);
+  // store the hash value in this variable
+  // uint64_t digest[2] = {0, 0};
+  // INIT SHA256 
+  
+  uint32_t state[8] = {
+    0x6a09e667, 0xbb67ae85, 0x3c6ef372, 0xa54ff53a,
+    0x510e527f, 0x9b05688c, 0x1f83d9ab, 0x5be0cd19
+  };
+
+
+
+  // hash a long message (for now it's a series of zeros)
+  gettimeofday(&begin, 0);
+  for (size_t i=0; i<n_of_blocks; ++i){
+    sha256_process_x86_single(state, M);
+    // truncate_state_get_digest(digest, &ctx, n_of_bits);
+
+    
+    /// ------------ DISTINGUISHED POINTS ------------------------- ///
+    /// If distinguished points feature was enabled  during compile ///
+    /// time. 
+    #ifdef DISTINGUISHED_POINTS
+    // we skip hashes
+    if ( (digest[0]&DIST_MASK) != 0) 
+      continue; // skip this element
+    #endif
+
+  }
+
+
+
+  gettimeofday(&end, 0);
+  seconds = end.tv_sec - begin.tv_sec;
+  microseconds = end.tv_usec - begin.tv_usec;
+  elapsed = seconds + microseconds*1e-6;
+
+  float hash_per_sec = (float) n_of_blocks / elapsed;
+  printf("sha256-x86\nelapsed=%fsec, %f hash/sec≈2^%f \n", elapsed, hash_per_sec, log2(hash_per_sec));
+  return hash_per_sec;
+}
+
 
 
 void filling_rate_time(size_t n_of_blocks, float alpha, FILE* fp){
@@ -134,12 +189,15 @@ int main(int argc, char* argv[]){
   fprintf(fp, "alpha, insert, lookup\n"
 	  "N=%lu\n", nelements);
   fclose(fp);
+
+  benchmark_sha256_x86();
+  benchmark_sha256();
   
-  for (float i=0.5; i<0.99; i += 0.01){
-    FILE* fp = fopen("log/benchmark_dict", "a");
-    filling_rate_time(nelements, i, fp);
-    fclose(fp);
-  }
+  /* for (float i=0.5; i<0.99; i += 0.01){ */
+  /*   FILE* fp = fopen("log/benchmark_dict", "a"); */
+  /*   filling_rate_time(nelements, i, fp); */
+  /*   fclose(fp); */
+  /* } */
 }
 
 
