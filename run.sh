b714ab6d93390e36d6e70ad34d15e3de5ba9a4bc
Ahmed Alharbi <akaalharbi@protonmail.com>
2023-01-13 16:45:01 +0100

    run script

--- run.sh
+++ run.sh
@@ -2,6 +2,6 @@ make purge
 make
 ./phase_i
 cp data/send/digests/* data/receive/digests/
-mpirun --oversubscribe -np 20
+mpirun --oversubscribe -np 20 ./phase_ii
 cat data/send/messages/* data/receive/messages/archive
 ./phase_iii
