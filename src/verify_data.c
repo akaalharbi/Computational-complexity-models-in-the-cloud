80eb43c0d6527c93786b227a104ffbdd057a687d
Ahmed Alharbi <48549687+akaalharbi@users.noreply.github.com>
2023-02-21 23:22:45 +0100

    Verify the correctness of states manually if the program stopped then reran

---
+++ src/verify_data.c
@@ -0,0 +1,77 @@
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "config.h"
+#include "hash.h"
+#include "numbers_shorthands.h"
+#include "util_char_arrays.h"
+
+int check_hashes_interval(const WORD_TYPE state_befoe[NWORDS_STATE],
+			  const WORD_TYPE state_after[NWORDS_STATE],
+			  const CTR_TYPE ctr){
+
+
+  u8 M[HASH_INPUT_SIZE] = {0};
+  /* We don't want to modify arguments */
+  WORD_TYPE state[NWORDS_STATE];
+  memcpy(state, state_befoe, HASH_STATE_SIZE);
+  
+  /* register counter in M */
+  ((u64*)M)[0] = ctr;
+
+  for (size_t i=0; i<INTERVAL+2; ++i) {
+
+    hash_single(state, M);
+    ++( ((u64*)M)[0]) ;
+
+    if (memcmp(state, state_after, HASH_STATE_SIZE) == 0){
+      printf("i=%lu\n",i );
+      print_char((u8*) state, HASH_STATE_SIZE);
+      print_char((u8*) state_after, HASH_STATE_SIZE);
+
+    }
+  }
+
+  
+  return (memcmp(state, state_after, HASH_STATE_SIZE) == 0);
+  
+}
+// @todo discovered a bug in when phase_i rerun again
+// it will write the last state twice
+
+int main(int argc, char *argv[])
+{
+
+  /* if (argc != 2) */
+  /*   return 0; */
+
+  u64 state_number = 1630;
+  u64 ctr = state_number*INTERVAL;
+  
+  WORD_TYPE state_before[NWORDS_STATE];
+  WORD_TYPE state_after[NWORDS_STATE];
+  
+  FILE* fp = fopen("data/states", "r");
+
+  fseek(fp, (HASH_STATE_SIZE)*(state_number), SEEK_CUR);
+  fread(state_before, 1, HASH_STATE_SIZE,  fp);
+
+  fread(state_after, 1,  HASH_STATE_SIZE, fp);
+
+  print_char((u8*) state_before, HASH_STATE_SIZE);
+  print_char((u8*) state_after, HASH_STATE_SIZE);
+  printf("ctr=%llu, INTERVAL=%llu\n", ctr, INTERVAL);
+  puts("Going to check...");
+
+  
+  int is_corrupt = (0 == check_hashes_interval(state_before,
+					       state_after,
+					       ctr));
+
+  printf("Found a corrupt state? %d\n", is_corrupt);
+
+  fclose(fp);
+  return 0;
+  
+}
